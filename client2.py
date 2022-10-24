c35935bd120b4e1f9ffeb6c6b3991e4725706762
Paul Bückhard <104130878+PaulBueckhard@users.noreply.github.com>
2022-10-24 14:25:34 +0200

    Add files via upload

---
+++ client2.py
@@ -0,0 +1,14 @@
+import websockets
+import asyncio
+import json
+
+async def listen():
+    url = "wss://api.pawn-hub.de/"
+
+    async with websockets.connect(url) as ws:
+        await ws.send(json.dumps({"type": "connect-attendee", "host": "0486", "code": "$CODE"}))
+        while True:
+            msg = await ws.recv()
+            print(msg)
+
+asyncio.get_event_loop().run_until_complete(listen())
