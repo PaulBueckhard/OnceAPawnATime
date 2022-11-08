46e11ee6456ac1b2d2cfdca977dfd8403cf087fc
Paul Bückhard <104130878+PaulBueckhard@users.noreply.github.com>
2022-11-08 13:43:00 +0100

    Update client.py

--- client.py
+++ client.py
@@ -22,36 +22,40 @@ async def listen():
             if string["type"] == "verify-attendee-request" and string["code"] == str(code):
                 clientId = string["clientId"]
                 await ws.send(json.dumps({"type": "accept-attendee-request", "clientId": clientId}))
+
+            if (string["type"] == "matched" and string["fen"] in msg) or (string["type"] == "receive-move"):
                 await ws.send(json.dumps({"type": "get-board"}))
+                msg = await ws.recv()
+                string = json.loads(msg)
+                
                 if "fen" in msg:
                     fen = string["fen"]
 
-                    if string["type"] == "matched" and string["fen"] in msg:
-                        def fen_to_board(fen):
-                            board = []
-                            for row in fen.split('/'):
-                                brow = []
-                                for c in row:
-                                    if c == ' ':
-                                        break
-                                    elif c in '12345678':
-                                        brow.extend( ['--'] * int(c) )
-                                    elif c == 'p':
-                                        brow.append( 'bp' )
-                                    elif c == 'P':
-                                        brow.append( 'wp' )
-                                    elif c > 'Z':
-                                        brow.append( 'b'+c.upper() )
-                                    else:
-                                        brow.append( 'w'+c )
+                def fen_to_board(fen):
+                    board = []
+                    for row in fen.split('/'):
+                        brow = []
+                        for c in row:
+                            if c == ' ':
+                                break
+                            elif c in '12345678':
+                                brow.extend( ['--'] * int(c) )
+                            elif c == 'p':
+                                brow.append( 'bp' )
+                            elif c == 'P':
+                                brow.append( 'wp' )
+                            elif c > 'Z':
+                                brow.append( 'b'+c.upper() )
+                            else:
+                                brow.append( 'w'+c )
 
-                                board.append( brow )
-                            return board
-                        pprint( fen_to_board(fen) )
+                        board.append( brow )
+                    return board
+                pprint( fen_to_board(fen) )
 
 
             
             
 asyncio.get_event_loop().run_until_complete(listen())
 
-## type: get-board
+## await ws.send(json.dumps({"type": "get-board"}))
